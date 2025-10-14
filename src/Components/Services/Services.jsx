94e56a16bc5e6f3f86e1a3c3e12714cb357e1c40
mahianmahin <mahianmahin@yahoo.com>
2025-10-15 00:46:06 +0600

    filter options changes made

--- src/Components/Services/Services.jsx
+++ src/Components/Services/Services.jsx
@@ -260,15 +260,15 @@ const Services = (props) => {
         {/* Mobile Filter Button - Only visible on small screens when filters are hidden */}
         <div className="flex justify-end lg:hidden mb-4">
           {!showMobileFilters && (
-            // <button onClick={() => setShowMobileFilters(true)} className="w-20 bg-[#930B31] text-white font-semibold py-2 px-4 rounded-xl shadow-lg hover:bg-red-800 transition-all duration-300 transform hover:scale-[1.02] flex items-center justify-center gap-3">
-            //   <svg className="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-            //     <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M3 4a1 1 0 011-1h16a1 1 0 011 1v2.586a1 1 0 01-.293.707l-6.414 6.414a1 1 0 00-.293.707V17l-4 4v-6.586a1 1 0 00-.293-.707L3.293 7.207A1 1 0 013 6.5V4z"></path>
-            //   </svg>
-            //   Filter
-            // </button>
-            <div className="mr-2 cursor-pointer" onClick={() => setShowMobileFilters(true)}>
-              <span className="text-[#930B31] underline">USE FILTERS</span>
-            </div>
+            <button onClick={() => setShowMobileFilters(true)} className="w-20 mr-2 bg-[#930B31] text-white font-semibold py-2 px-4 rounded-xl shadow-lg hover:bg-red-800 transition-all duration-300 transform hover:scale-[1.02] flex items-center justify-center gap-3">
+              <svg className="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M3 4a1 1 0 011-1h16a1 1 0 011 1v2.586a1 1 0 01-.293.707l-6.414 6.414a1 1 0 00-.293.707V17l-4 4v-6.586a1 1 0 00-.293-.707L3.293 7.207A1 1 0 013 6.5V4z"></path>
+              </svg>
+              {/* Filter */}
+            </button>
+            // <div className="mr-2 cursor-pointer" onClick={() => setShowMobileFilters(true)}>
+            //   <span className="text-[#930B31] underline">USE FILTERS</span>
+            // </div>
             )}
           </div>
 
