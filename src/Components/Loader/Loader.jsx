3c99274755638d6d39fcaf5c26c27bff132138bd
mahianmahin <mahianmahin@yahoo.com>
2026-03-04 18:22:44 +0600

    report2 step 6 done

--- src/Components/Loader/Loader.jsx
+++ src/Components/Loader/Loader.jsx
@@ -1,11 +1,14 @@
-import loaderGif from '../../assets/custom_loader.gif';
-
 const Loader = () => {
   return (
-    <div className="fixed top-0 left-0 w-full h-full flex items-center justify-center">
-      <img src={loaderGif} alt="Loading..." className="w-24 h-24" />
+    <div className="fixed top-0 left-0 w-full h-full flex items-center justify-center bg-white/80 z-50">
+      <div className="relative">
+        {/* Outer ring */}
+        <div className="w-16 h-16 rounded-full border-4 border-gray-200" />
+        {/* Spinning arc */}
+        <div className="absolute top-0 left-0 w-16 h-16 rounded-full border-4 border-transparent border-t-[#930B31] animate-spin" />
+      </div>
     </div>
   );
 };
 
-export default Loader; 
+export default Loader;
